523a2524d5ed92761fbd79b8a7f489a4164cb1aa
Rel <errelealviar17@gmail.com>
2026-05-20 22:20:05 +0800

    fix: use dynamic port for Render

--- sheety_handler.py
+++ sheety_handler.py
@@ -1,6 +1,15 @@
 import requests
 from datetime import datetime
-from config import SHEETY_ENDPOINT, HEADER, SHEET_NAME
+import os
+
+TOKEN = os.environ.get("TOKEN")
+PROJECT_NAME = os.environ.get("PROJECT_NAME")
+SHEET_NAME = os.environ.get("SHEET_NAME")
+USERNAME = os.environ.get("USERNAME")
+PASSWORD = os.environ.get("PASSWORD")
+
+SHEETY_ENDPOINT = f"https://api.sheety.co/{TOKEN}/{PROJECT_NAME}/{SHEET_NAME}"
+HEADER = {"Authorization": f"Basic {os.environ.get('AUTH_TOKEN')}"}
 
 def post_to_sheety(parsed_data):
     today = datetime.now().strftime("%d/%m/%Y")
